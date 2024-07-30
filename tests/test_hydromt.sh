daa6e02736b69ec99058d3f324fd06064003a444
iferrario <iferrario@eurac.edu>
2024-07-30 14:13:43 +0200

    test hydromt complete

--- tests/test_hydromt.sh
+++ tests/test_hydromt.sh
@@ -4,9 +4,9 @@ echo $PWD
 
 HYDROMT_DIR=$PWD/workflows/hydromt
 
-docker build --no-cache -t hydromt-test $HYDROMT_DIR
+docker build --no-cache -f $HYDROMT_DIR/TestDockerfile -t hydromt-test $HYDROMT_DIR
 
-docker container run -v /mnt/CEPH_PROJECTS/InterTwin/hydrologic_data:/data \
+docker container run \
     -it hydromt-test hydromt build wflow model \
     -r "{'subbasin': [11.4750, 46.8720]}" \
     -d data_catalog.yaml -i wflow.ini -vvv
