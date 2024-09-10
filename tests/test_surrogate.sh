e6d9c070c909fbdccf266a1081eb64403f6d8146
iacopoff <iacopo.ff@gmail.com>
2024-09-10 11:16:25 +0200

    itwinai surrogate

--- tests/test_surrogate.sh
+++ tests/test_surrogate.sh
@@ -2,6 +2,9 @@
 
 echo $PWD
 
+
+CMD="itwinai exec-pipeline --config config.yaml --pipe-key training_pipeline"
+
 SURR_DIR=$PWD/workflows/surrogate
 
 if [ ! -d "$SURR_DIR/tmp" ]; then
@@ -10,5 +13,13 @@ fi
 
 docker build --no-cache -f $SURR_DIR/TestDockerfile -t surrogate-test $SURR_DIR
 
-docker container run -it --rm surrogate-test itwinai exec-pipeline --config ./use-case/config.yaml --pipe-key training_pipeline
-
+if [ -z "$1" ]; then
+	# CPU
+	docker run -it --rm \
+	    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+	    surrogate-test /bin/bash -c "cd ./use-case && $CMD"
+elif [ "$1" == "gpu" ]; then
+	docker run -it --rm \
+		--gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+		surrogate-test /bin/bash -c "cd ./use-case && $CMD"
+fi
