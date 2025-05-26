ddafdf6591a74b8b6c96f5a72494cde275644c63
iacopoff <iacopoff@gmail.com>
2025-05-26 16:36:33 +0200

    update test surrogate

--- tests/test_surrogate_local.sh
+++ tests/test_surrogate_local.sh
@@ -24,18 +24,13 @@ if [ -z "$1" ]; then
 		-e MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI -e MLFLOW_TRACKING_USERNAME=$MLFLOW_TRACKING_USERNAME \
 		-e MLFLOW_TRACKING_INSECURE_TLS=$MLFLOW_TRACKING_INSECURE_TLS -e MLFLOW_TRACKING_PASSWORD=$MLFLOW_TRACKING_PASSWORD \
 		-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY \
-	    surrogate-test:latest /bin/bash -c '
-		    echo "Inside container:"
-			echo "Working dir: $(pwd)"
-			echo "Contents of /app/use-case:"
-			ls -lah /app/use-case
-			echo "Contents of /data:"
-			ls -lah /data
-			echo "Environment:"
-			env
-			echo "Starting pipeline..."
-			cd ./use-case && itwinai exec-pipeline --config-name training_local
-	'
+	    surrogate-test:latest /bin/bash -c "python ./use-case/gen_surr_config.py --config "./use-case/training_local.yaml" --output "./use-case/training_local_updated.yaml" \
+		--cp 'train_temporal_range=["2015-01-01", "2016-12-01"]' \
+		--cp 'valid_temporal_range=["2015-01-01", "2016-12-01"]' \
+		--cp 'test_temporal_range=["2015-01-01", "2016-12-01"]' \
+		&& cat ./use-case/training_local_updated.yaml \
+		&& itwinai exec-pipeline --config-dir ./use-case --config-name training_local_updated "
+
 elif [ "$1" == "gpu" ]; then
 	echo "Running with GPU"
 	docker run -it --rm \
@@ -55,7 +50,7 @@ elif [ "$1" == "gpu" ]; then
 			echo "Environment:"
 			env
 			echo "Starting pipeline..."
-			cd ./use-case && itwinai exec-pipeline --config-name training_local
+			itwinai exec-pipeline --config-dir ./use-case --config-name training_local
 	'
 fi
 
