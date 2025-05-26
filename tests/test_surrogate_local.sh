aa44cb687f5d5111454845a6378f0a5f13c54d4e
iacopoff <iacopoff@gmail.com>
2025-05-26 16:42:51 +0200

    fix entry point test surrgoate

--- tests/test_surrogate_local.sh
+++ tests/test_surrogate_local.sh
@@ -19,17 +19,23 @@ docker build --no-cache -f $SURR_DIR/Dockerfile -t surrogate-test $SURR_DIR
 if [ -z "$1" ]; then
 	# CPU
 	docker run -it --rm \
-		-v $PWD/tests/tmp:/data \
-	    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-		-e MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI -e MLFLOW_TRACKING_USERNAME=$MLFLOW_TRACKING_USERNAME \
-		-e MLFLOW_TRACKING_INSECURE_TLS=$MLFLOW_TRACKING_INSECURE_TLS -e MLFLOW_TRACKING_PASSWORD=$MLFLOW_TRACKING_PASSWORD \
-		-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY \
-	    surrogate-test:latest /bin/bash -c "python ./use-case/gen_surr_config.py --config "./use-case/training_local.yaml" --output "./use-case/training_local_updated.yaml" \
-		--cp 'train_temporal_range=["2015-01-01", "2016-12-01"]' \
-		--cp 'valid_temporal_range=["2015-01-01", "2016-12-01"]' \
-		--cp 'test_temporal_range=["2015-01-01", "2016-12-01"]' \
+	-v "$PWD/tests/tmp:/data" \
+	--ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+	-e MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI \
+	-e MLFLOW_TRACKING_USERNAME=$MLFLOW_TRACKING_USERNAME \
+	-e MLFLOW_TRACKING_INSECURE_TLS=$MLFLOW_TRACKING_INSECURE_TLS \
+	-e MLFLOW_TRACKING_PASSWORD=$MLFLOW_TRACKING_PASSWORD \
+	-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY \
+	-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY \
+	surrogate-test:latest /bin/bash -c "python ./use-case/gen_surr_config.py \
+		--config './use-case/training_local.yaml' \
+		--output './use-case/training_local_updated.yaml' \
+		--cp 'train_temporal_range=[\"2015-01-01\", \"2016-12-01\"]' \
+		--cp 'valid_temporal_range=[\"2015-01-01\", \"2016-12-01\"]' \
+		--cp 'test_temporal_range=[\"2015-01-01\", \"2016-12-01\"]' \
 		&& cat ./use-case/training_local_updated.yaml \
-		&& itwinai exec-pipeline --config-dir ./use-case --config-name training_local_updated "
+		&& itwinai exec-pipeline --config-dir ./use-case --config-name training_local_updated"
+
 
 elif [ "$1" == "gpu" ]; then
 	echo "Running with GPU"
